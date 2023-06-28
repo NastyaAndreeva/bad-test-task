d1a72240ba88890b51ff031cdb7e64d83ca693f5
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2023-06-28 14:41:54 +0300

    deleted comments

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Вам потрібно розробити алгоритм програми, яка повинна виконувати наступне:
-// - програма приймає на вхід довільний текст і знаходить в кожному слові цього тексту найперший символ, який більше НЕ повторюється в аналізуємому слові
-// - далі із отриманого набору символів програма повинна вибрати перший унікальний (тобто той, який більше не зустручається в наборі) і повернути його.
 const textString =
   'The Tao gave birth to machine language.  Machine language gave birth to the assembler. he assembler gave birth to the compiler.  Now there are ten thousand languages. Each language has its purpose, however humble.  Each language expresses the Yin and Yang of software.  Each language has its place within the Tao. But do not program in COBOL if you can avoid it.         Geoffrey James, "The Tao of Programming"';
 
@@ -27,6 +24,4 @@ const getUniqueLetter = (text) => {
   return null;
 };
 
-// const result = getUniqueLetter("I love JavaScript");
-const result = getUniqueLetter(textString);
-console.log("result: ", result);
+console.log("result: ", getUniqueLetter(textString));
